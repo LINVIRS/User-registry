c0238b79e56ea9a625863a541b60b9a4ba7a7408
LINVIRS <1987153442@qq.com>
2021-12-10 09:55:45 +0800

    后端框架搭建

---
+++ dao/src/main/java/com/wl/api/exception/InvalidSMSCodeException.java
@@ -0,0 +1,17 @@
+package com.wl.api.exception;
+
+
+import com.wl.api.result.ResultCode;
+
+/**
+ * @Company: NJBandou
+ * @Author: Chen Pan
+ * @Description:
+ * @Date: 2018/7/5 下午3:32
+ */
+public class InvalidSMSCodeException extends ApiException{
+
+    public InvalidSMSCodeException() {
+        super(ResultCode.INVALID_SMS_CODE.getMsg(), ResultCode.INVALID_SMS_CODE.getCode());
+    }
+}
